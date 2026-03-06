999d4b707ab527d63564c2935d5007f95843c457
AnthonyHa14 <a_halaji@live.concordia.ca>
2026-03-06 10:56:03 -0500

    database set up.py

--- databse.py
+++ databse.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 class DatabaseManager:
 
     def __init__(self, db_name="schedule.db"):
@@ -8,25 +9,21 @@ class DatabaseManager:
 
     def create_tables(self):
 
-        self.cursor.execute("""
-        CREATE TABLE IF NOT EXISTS Users (
-            userId INTEGER PRIMARY KEY,
-            firstName TEXT,
-            lastName TEXT,
-            email TEXT
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS Users (userId INTEGER PRIMARY KEY, firstName TEXT, lastName TEXT, email TEXT)"
         )
-        """)
 
-        self.cursor.execute("""
-        CREATE TABLE IF NOT EXISTS Schedule (
-            scheduleId INTEGER PRIMARY KEY,
-            weekStartDate TEXT,
-            weekEndDate TEXT,
-            scheduleStatus TEXT
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS Schedule (scheduleId INTEGER PRIMARY KEY, weekStartDate TEXT, weekEndDate TEXT, scheduleStatus TEXT)"
         )
-        """)
 
         self.connection.commit()
 
     def close(self):
         self.connection.close()
+
+
+if __name__ == "__main__":
+    db = DatabaseManager()
+    db.create_tables()
+    db.close()
